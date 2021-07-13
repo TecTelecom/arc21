6d82d767d5dd15d298827f65e54de032d04c5bc3
Beatriz Paz Faria <estudo.bea@gmail.com>
2021-07-13 12:51:19 +0000

    criando pasta backend no arquivo config.py

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ import logging
 # the options in the more complete config-template.py from here:
 # https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
 
-BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'Discord'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
-BOT_DATA_DIR = r'/workspace/arc21/data'
-BOT_EXTRA_PLUGIN_DIR = r'/workspace/arc21/plugins'
+BOT_DATA_DIR = r'data'
+BOT_EXTRA_PLUGIN_DIR = r'plugins'
+BOT_EXTRA_BACKEND_DIR = r'backend'
 
-BOT_LOG_FILE = r'/workspace/arc21/errbot.log'
+BOT_LOG_FILE = r'errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
-BOT_ADMINS = ('@CHANGE_ME', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!
+BOT_ADMINS = ('@CHANGE_ME', )  
